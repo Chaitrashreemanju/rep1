3bdd72e6f9422423dd611505d478e0d27ba5dc31
Chaitrashreemanju <chaitramanju23980@gmail.com>
2024-09-29 13:17:13 +0530

    range_for

---
+++ for_range.py
@@ -0,0 +1,10 @@
+# n NATURAL NUMBERS
+end=int(input("Enter the number:"))
+for num in range(1,end+1):
+    print(num)
+
+
+# n EVEN NUMBERS 
+end= int(input("Enter the number:"))
+for num in range(1,end+1,2):
+    print(num)   
